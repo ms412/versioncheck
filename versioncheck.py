a070e0c02f28d961f391841e297062d1fbe3c3ab
Markus Schiesser <M.Schiesser@gmail.com>
2014-02-26 09:25:06 +0100

    remove debug information

--- versioncheck.py
+++ versioncheck.py
@@ -17,23 +17,26 @@ import csv
 
 
 class CSVfile(object):
+    '''
+    opens CSV file and creates a object instance for each board
+    '''
     
     def __init__(self,csvfile, resultfile = 'result.csv'):
 
         self._boardlist = []
         
-
- #       self._csvfile = csvfile
         self._resultfile = resultfile
         self._tempfile = 'tempfile.temp'
         
         self.Copyfile(csvfile,self._tempfile)
         
     def __del__(self):
- #       os.remove(self._tempfile)
+        ''' 
+        delete the tempfile if it exists
+        '''
+        
         if os.path.isfile(self._tempfile):
             os.remove(self._tempfile)
-           # print "Delete Object"
         
     def Copyfile(self, source, dest, buffer_size=1024*1024):
         """
@@ -56,9 +59,11 @@ class CSVfile(object):
         source.close()
         dest.close()
 
-
-        
     def RemoveHaeder(self):
+        '''
+        remove first ten lines of Inventory file as it get created by U2000
+        '''
+        
         readTempfile = file(self._tempfile)
         lines = readTempfile.readlines()
         readTempfile.close()
@@ -76,18 +81,24 @@ class CSVfile(object):
         return True
         
     def detectDelimiter(self,csvFile):
+        ''' 
+        detects the delimiter between lines in CVS file
+        either ',' or ';'
+        '''
+        
         with open(csvFile, 'r') as myCsvfile:
             header=myCsvfile.readline()
             if header.find(";")!=-1:
                 return ";"
             if header.find(",")!=-1:
                 return ","
-            #default delimiter (MS Office export)
         return ";"
 
     def OpenFile(self):
         '''
-        detect delimiter either ',' or ';'
+        open files
+        detects delimiter of CSV file
+        and creates Board instances for each line in file
         '''
         detectDelimiter = self.detectDelimiter(self._tempfile)
 
@@ -99,9 +110,10 @@ class CSVfile(object):
         return True
     
     def WriteFile(self,data):
-        
-  #      print data
-    #    print "Resultfile", self._resultfile
+        '''
+        writes result to file
+        expects a list of dictionaries
+        '''
         resultFile = csv.DictWriter(open(self._resultfile,"wb"),
                                 ["RESULT","DETAIL","BRD_TYPE", "BOM", "SERIAL", "NE_NAME","NE_TYPE", "RACK",
                                   "SLOT", "SW_EXP", "SW_ACT", "FPGA_EXP", "FPGA_ACT"])
@@ -113,15 +125,20 @@ class CSVfile(object):
         
         resultFile.writerows(data) 
         
-        
         return True
         
     def InstanceList(self):
+        '''
+        returns the list of pointers of instances of the Boards
+        '''
         return self._boardlist
 
 class Board(object):
     
     def __init__(self,dict):
+        ''' 
+        creates a board instance
+        '''
         self._BOM = dict.get('Board BOM Item')
         self._BRD_TYPE = dict.get('Board Type')
         self._BRD_SW = dict.get('Software Version')
@@ -146,7 +163,10 @@ class Board(object):
         return self._NE_TYPE
     
     def FPGA(self, version = 0):
-#        print "FPGA", self._FPGA
+        ''' 
+        returns the FPGA version if the version is not set
+        or overwritten by version if != 0
+        '''
         if version != 0:
             self._FPGA = version
         return self._FPGA
@@ -166,45 +186,27 @@ class Board(object):
 class Compare(object):
     
     def __init__(self,referenceList,inventoryList):
+        '''
+        class compares the reference with inventory list
+        '''
 
         self._referenceList = referenceList
         self._inventoryList = inventoryList
         self._result = []
-        
- #   def Search(self, item, value):
-
- #       result = getattr(item,value)()
-#        print "Search::Result", result
-  #      resultList = []
-        
- #       for referenceItem in self._referenceList:
-  #          referenceValue = getattr(referenceItem, value)()
- #           print "Search Reverence value", referenceValue
-#            if result.find(referenceValue) > -1:
-#                resultList.append(referenceItem)
-            
-  #          if result.find(referenceItem.value()) > -1:
-   #             resultList.append(referenceItem) 
-                
-  #      print "Search::resultLsit", resultList
- #       return resultList
     
     def Search(self, referenceList, item, value):
+        '''
+        search in reference list for value present in item instance and reference list
+        '''
 
         result = getattr(item,value)()
-    #    print "Search::Result", result
         resultList = []
         
         for referenceItem in referenceList:
             referenceValue = getattr(referenceItem, value)()
-     #       print "Search Reverence value", referenceValue
             if result.find(referenceValue) > -1:
                 resultList.append(referenceItem)
-            
-  #          if result.find(referenceItem.value()) > -1:
-   #             resultList.append(referenceItem) 
-                
-      #  print "Search::resultLsit", resultList
+
         return resultList
         
     def Filter(self):
@@ -223,12 +225,10 @@ class Compare(object):
             else board or NE not defined or multiple definitions of board in reference list
             '''
             referenceList = self.Search(self._referenceList,inventoryItem,'BRD_TYPE')
-  #          print "BoardType",len(referenceList)
             
             if len(referenceList) != 0:
                 referenceItem = self.Search(referenceList, inventoryItem,'NE_TYPE')
                 
-   #             print "NE Type",len(referenceList)
                 if len(referenceItem) == 1:
                     validBoard = True
                     resultmsg = 'OK'
@@ -252,11 +252,7 @@ class Compare(object):
             than verify FPGA and Software version of board
             '''    
             if validBoard == True:
-       #         print "FPGA Act", inventoryItem.FPGA(), "expected",referenceItem[0].FPGA()
-        #        if '/' in inventoryItem.FPGA():
-         #           print "heir"
-          #      if 'ZERO' in referenceItem[0].FPGA():
-           #         print "nnnn"
+
                 ''' 
                 if no FPGA on  board "/" and reference File = 'ZERO'
                 '''
@@ -270,14 +266,11 @@ class Compare(object):
                 elif inventoryItem.FPGA() == referenceItem[0].FPGA():
                     resultmsg = 'OK'
                     detailmsg = 'FPGA::OK'
-         #           print "FPGA found"
                 else:
                     resultmsg = 'NOK'
                     detailmsg = 'FPGA::FAILED'
-        #            print "FPGA not found", referenceItem[0].FPGA()
                 
                 if 'NOK' not in resultmsg:  
-#                if resultmsg.find('OK') != -1:
                     if inventoryItem.BRD_SW() == referenceItem[0].BRD_SW():
                         resultmsg = 'OK'
                         detailmsg = detailmsg + ' ' + 'SW::OK'
@@ -299,8 +292,7 @@ class Compare(object):
             else:
                 self._result.append({"RESULT":resultmsg,"DETAIL":detailmsg,"BRD_TYPE":inventoryItem.BRD_TYPE(),"BOM":inventoryItem.BOM(),
                                 "SERIAL":inventoryItem.SERIAL(),"NE_NAME":inventoryItem.NE_NAME(),"NE_TYPE":ne_type,"RACK":inventoryItem.RACK(),"SLOT":inventoryItem.SLOT()}) 
-                
- #           print self._result
+
         return self._result
            
 if __name__ == '__main__':
@@ -309,21 +301,15 @@ if __name__ == '__main__':
         referenceFile = sys.argv[1]
         inventoryFile = sys.argv[2]
         resultFile = sys.argv[3]
- #       print "Debug Parameter", referenceFile, inventoryFile, resultFile
     else:
-        resultFile = 'result.csv'
-        inventoryFile = 'inventory.csv'
-        referenceFile = 'reference.csv'
-        print "parameter missing"
+        print "Start tool with following parameter./versioncheck.py [Reference File][Inventory File][Output File]"
     
     reference = CSVfile(referenceFile)
     reference.OpenFile()
- #   print "reference", len(reference.InstanceList())
     
     inventory = CSVfile(inventoryFile,resultFile)
     inventory.RemoveHaeder()
     inventory.OpenFile()
-  #  print "Inventory",len(inventory.InstanceList())
     
     compare = Compare(reference.InstanceList(), inventory.InstanceList())
     resultdata = compare.Filter()
